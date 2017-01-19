f552632c2f127fd7dfda0d4940e522bc7055904d
k2263 <k2263@student.jamk.fi>
2017-01-19 19:14:31 +0200

    teth 09 started

---
+++ teht09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Anna luku > ");
+                int luku = int.Parse(Console.ReadLine());
+
+
+                if (luku != 0) ;
+                int summa = int.Parse(Console.ReadLine());
+                summa = luku + luku;
+
+                Console.Write("Lukujen summa on " + summa + "\n");
+            }
+        }
+    }
+}
