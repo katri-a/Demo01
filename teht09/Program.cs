f9f33ab4a834d7bf9061d48cfc4eed983b8e82e9
k2263 <k2263@student.jamk.fi>
2017-01-24 17:15:58 +0200

    teht10 done

--- teht09/Program.cs
+++ teht09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,3 +26,4 @@ namespace teht09
         }
     }
 }
+
---
+++ teht10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            for (int i = 0; i < values.Length; i++) 
+            if (values[i] % 2 == 0)Console.Write("HEP\n");
+
+
+        }
+    }
+}
