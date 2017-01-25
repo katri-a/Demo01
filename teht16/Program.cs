1f22d4afbfd0ce69be7b16e4453f3848eea93d5a
k2263 <k2263@student.jamk.fi>
2017-01-25 23:06:34 +0200

    teht16 kesken

---
+++ teht16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rand = new Random();
+
+            int sLuku = rand.Next(0, 101);
+            int arvaus = 0;
+           
+            while (sLuku != arvaus) {
+
+                Console.WriteLine("Arvaa luku: ");
+                 arvaus = int.Parse(Console.ReadLine());
+                if (arvaus > sLuku)
+                {
+                    Console.Write("Luku on pienempi."); continue;
+                }
+                else if (arvaus < sLuku)
+                {
+                    Console.Write("Luku on suurempi."); continue;
+                }
+                else if (arvaus == sLuku)
+                {
+                    Console.Write("Arvasin oikein.");
+                }
+            }
+        }
+    }
+}
